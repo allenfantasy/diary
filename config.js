fa6e4395753475b92c03e74fbd52a03913546597
Allen Wu <allenwu1018@gmail.com>
2016-02-05 00:51:30 +0800

    t

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
   author: {
-    name: "Joyee Cheung",
-    url: "https://github.com/joyeecheung"
+    name: "Allen Wu",
+    url: "https://github.com/allenfantasy"
   },
   repo: {  // for the markdown source
-    content: "https://github.com/joyeecheung/my-tech-diary",
+    content: "https://github.com/allenfantasy/my-tech-diary",
     as_submodule: 'diary'
   },
   base_url: '/diary',  // remote directory for the website
-  ga_id: 'UA-63818301-1',  // Optional: Universal Google Analytics ID
+  //ga_id: 'UA-63818301-1',  // Optional: Universal Google Analytics ID
   title: 'My Tech Diary',
   lang: 'en'
 }
